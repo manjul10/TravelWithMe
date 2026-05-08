e04b8367265381fd84e9a83e88dd81c9b08d9f52
mahigya122 <mahigyadahal@gmail.com>
2026-05-08 20:35:18 +0545

    login page fixed

--- src/layout/Login.tsx
+++ src/layout/Login.tsx
@@ -1,54 +1,56 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/authSlice";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const dispatch = useDispatch();
-    const navigation = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigation = useNavigate();
 
-const handleLogin = (e: React.FormEvent) => { 
-  e.preventDefault();
- 
-  if (email === "user@example.com" && password === "password") {   
-    dispatch(login(email));
-    navigation("/"); 
-  }
-};
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
 
-  return (  
-    <div className = "min-h-screen flex items-center justify-center bg-gray-100">
-      <form 
-      onSubmit={handleLogin} 
-      className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-      <h1 className= "text-2xl font-bold mb-4">Login</h1>
+    if (email === "user@example.com" && password === "password") {
+      dispatch(login(email));
+      navigation("/");
+    }
+  };
 
-      <input
-      type="email"
-      placeholder="Email"
-      value={email}
-      onChange={(e) => setEmail(e.target.value)}
-      className="w-full p-3 mb-4 border rounded"
-      />
-
-      <input
-      type="password"
-      placeholder="Password"
-      value={password}
-      onChange={(e) => setPassword(e.target.value)}
-      className="w-full p-3 mb-4 border rounded"
-      />
-
-      <button 
-      type="submit"
-      className="w-full bg-blue-500 text-white p-3 rounded hover:bg-blue-600"
+  return (
+    <div className="flex items-center justify-center py-12">
+      <form
+        onSubmit={handleLogin}
+        className="bg-white p-8 rounded-lg shadow-md w-full max-w-md"
       >
-      Login
-      </button>
+        <h1 className="text-2xl font-bold mb-4">Login</h1>
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="w-full p-3 mb-4 border rounded"
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full p-3 mb-4 border rounded"
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-blue-500 text-white p-3 rounded hover:bg-blue-600"
+        >
+          Login
+        </button>
       </form>
     </div>
   );
 };
+
 export default Login;
