fe2edbadda5d7cdbbf685d8091789b08bf4bed63
manjul10 <manjulg11@gmail.com>
2026-05-08 16:59:30 +0545

    refinded homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,29 @@
 import bgImage from "../assets/bg.jpg";
+import Login from "../layout/Login";
 const Home = () => {
   return (
-    <div className="min-h-screen px-6 py-16 bg-white grid grid-cols-2 gap-16">
-      <div>
-        <img src={bgImage}  />
-      </div>
-      <div className="flex flex-col mb-8">
-        <h1>About WorldWide.</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo est
-          dicta illum vero culpa cum quaerat architecto sapiente eius non
-          soluta, molestiae nihil laborum, placeat debitis, laboriosam at fuga
-          perspiciatis?
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis
-          doloribus libero sunt expedita ratione iusto, magni, id sapiente sequi
-          officiis{" "}
-        </p>
+    <div className="w-full p-2  bg-white ">
+      <div className="relative w-full h-[90vh] overflow-hidden rounded-lg">
+        <img
+          src={bgImage}
+          className="object-cover object-center h-full w-full brightness-50 opacity-90"
+        />
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+          <h1 className="text-5xl font-bold mb-4 ">You travel the world.</h1>
+          <h1 className="text-5xl font-bold mb-4 ">
+            WorldWise keeps track of your adventures.
+          </h1>
+          <p className="text-xl">
+            A world map that tracks your footsteps into every city you can think
+            of.{" "}
+          </p>
+          <p className="text-xl">
+            Never forget your wonderful experiences, and show your friends how
+            you have wandered the world.
+          </p>
+        </div>
+
+      <Login/>
       </div>
     </div>
   );
